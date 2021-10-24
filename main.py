b88c3c313229e2bbdfce2e83dda0888b7c90ec0b
James Rivett <jrivett@students.kennesaw.edu>
2021-10-23 21:08:14 -0400

    Looks like it's working

--- main.py
+++ main.py
@@ -18,24 +18,57 @@ def carveSeam(start, pixels):
 
         if lDiff > rDiff:
             newRow = deepcopy(row)
-            #del newRow[x]
+            del newRow[x]
+            x = x - 1
+        if lDiff < rDiff:
+            newRow = deepcopy(row)
+            del newRow[x]
+            x = x + 1
+        else:
+            num = random.randint(0, 1)
+            if num == 0:
+                newRow = deepcopy(row)
+                del newRow[x]
+                x = x - 1
+            if num == 1:
+                newRow = deepcopy(row)
+                del newRow[x]
+                x = x + 1
+ 
+        newPixels += [newRow]
+
+    pixArr = np.array(newPixels)
+    return pixArr
+
+def showSeam(start, pixels):
+    x = start
+    lpixels = pixels.tolist()
+    newPixels = []
+
+    for row in lpixels:
+        mid = row[x]
+        left = row[x - 1]
+        right = row[x + 1]
+
+        lDiff = findDiff(mid, left)
+        rDiff = findDiff(mid, right)
+
+        if lDiff > rDiff:
+            newRow = deepcopy(row)
             newRow[x] = [255, 0, 0]
             x = x - 1
         if lDiff < rDiff:
             newRow = deepcopy(row)
-            #del newRow[x]
             newRow[x] = [255, 0, 0]
             x = x + 1
         else:
             num = random.randint(0, 1)
             if num == 0:
                 newRow = deepcopy(row)
-                #del newRow[x]
                 newRow[x] = [255, 0, 0]
                 x = x - 1
             if num == 1:
                 newRow = deepcopy(row)
-                #del newRow[x]
                 newRow[x] = [255, 0, 0]
                 x = x + 1
  
@@ -56,8 +89,8 @@ def main():
     image = Image.open('img/flowers.jpg')
     pix = np.asarray(image)
 
-    for i in range(20):
-        pix = carveSeam(200 + i, pix)
+    for i in range(int(len(pix[0]) / 11)):
+        pix = carveSeam((i * 10) , pix)
 
     image2 = Image.fromarray((pix).astype(np.uint8))
     image2.show()
